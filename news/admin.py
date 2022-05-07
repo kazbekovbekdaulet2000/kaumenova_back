dbad629f4d4bb6691f2b44be4ca1990d5ff91c27
Bekdaulet <kazbekov.bekdaulet2000@gmail.com>
2022-05-07 14:26:06 +0600

    changes

--- news/admin.py
+++ news/admin.py
@@ -10,6 +10,10 @@ class NewsAdmin(admin.ModelAdmin):
 
 
 class BroadcastAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
     search_fields = ['username', 'phone_number']
     list_display = ['username', 'phone_number']
 
